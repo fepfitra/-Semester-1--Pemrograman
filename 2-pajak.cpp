3dad4fd9b8d09601fe99d626c183f8df216edef4
fitrafepUGM <fitriansyahekaputra2001@mail.ugm.ac.id>
2022-09-15 07:46:37 +0700

    pajak

---
+++ 2-pajak.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    //deklarasi variabel
+    string item;
+    int harga, total;
+
+    //masukkan nama item
+    cout << "Nama item : ";
+    cin >> item;;
+
+    cout << "Harga barang : ";
+    cin >> harga;
+
+    //informasi pajak
+    cout << "Pajak: 11%" << endl;
+
+    //hitung
+    total = harga*1.11;
+
+    //cetak
+    cout << total;
+    
+}   
