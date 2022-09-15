d595bb59453758190c99edab805b0612e53999e8
fitrafepUGM <fitriansyahekaputra2001@mail.ugm.ac.id>
2022-09-15 21:59:03 +0700

    angka turun

---
+++ 4-angka-turun.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n = 7;
+    while (n > 1){
+        cout << n << endl;
+        n--;
+    }
+
+    // n=7;
+    // for (int i=n; i>1; i++){
+    //     cout << i << endl;
+    // }
+}
