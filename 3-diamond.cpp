30fbbc48bb288005add574f54c8ba18dd8ea70f8
fitrafepUGM <fitriansyahekaputra2001@mail.ugm.ac.id>
2022-09-15 21:57:03 +0700

    minor fix for compare

--- 3-diamond.cpp
+++ 3-diamond.cpp
@@ -6,11 +6,14 @@ int main(){
     int n;
     n = 5;
 
-    for(int i=1; i<=n; i++){
+    //hitung i dari 1 ke 5
+    for(int i = 1; i <= n; i++){
+        //hitung j dari i ke 5 
         for(int j = i; j < n; j++){
             cout << " ";
         }
 
+        //hitung j dari 1 ke i*2-1
         for(int j = 1; j <= i*2-1; j++){
             cout << "*";
         }
@@ -18,13 +21,17 @@ int main(){
         cout << endl;
     }
 
-    
-    for(int i=1; i<n; i++){
-        for(int j = 1; j<= i; j++){
+    //kurangi nilai n
+    n--;
+    // hitung i dari 1 ke 4
+    for(int i = 1; i <= n; i++){
+        //hitung j dari 1 ke i
+        for(int j = 1; j <= i; j++){
             cout << " ";
         }
 
-        for(int j = 1; j <= (n-i)*2-1; j++){
+        //hitung j dari 1 ke (n-i)*2+1
+        for(int j = 1; j <= (n-i)*2+1; j++){
             cout << "*";
         }
 
