8d3d9a209ba85c47dd2ce0b1d8b6e68b8932ce94
fitrafepUGM <fitriansyahekaputra2001@mail.ugm.ac.id>
2022-09-23 13:35:24 +0700

    is positive negative or zero

---
+++ 7-is-positive-negtive-or-zero.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Masukkan angka : ";
+    cin >> n;
+
+    if( n > 0){
+        cout << "Positif";
+    }else if(n = 0){
+        cout << "Nol";
+    }else{
+        cout << "Negatif";
+    }
+}
