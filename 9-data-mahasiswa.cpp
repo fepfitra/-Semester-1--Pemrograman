94193a9e9a633b06fdc85ddb90b36f89051c975e
fitrafepUGM <fitriansyahekaputra2001@mail.ugm.ac.id>
2022-10-11 16:37:55 +0700

    9-data-mahasiswa

---
+++ 9-data-mahasiswa.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    struct {
+        string nama;
+        int nim;
+    } mahasiswa[3];
+
+    cout << "Input : " << endl;
+
+    for( int i=0; i<3; i++){
+        cout << "Masukkan nama " << i+1 << " : ";
+        cin >> mahasiswa[i].nama;
+
+        cout << "Masukkan nim " << i+1 << " : ";
+        cin >> mahasiswa[i].nim;
+
+        cout << endl;
+    }
+
+    cout << "Output : " << endl;
+
+    for( int i=0; i<3; i++){
+        cout << "Nama [" << i+1 << "] : " << mahasiswa[i].nama << endl;
+
+        cout << "NIM  [" << i+1 << "] : " << mahasiswa[i].nim << endl;
+
+        cout << endl;
+    }
+}
